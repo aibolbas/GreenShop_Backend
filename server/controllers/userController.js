aba7c47cfd12dbf8031a2efbf4f194b671fd82e8
aibolbas <aibolkasym58@gmail.com>
2024-07-12 18:23:55 +0500

    first commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,6 +38,7 @@ const login = async(req,res)=>{
         console.error(err.message);
         res.status(500).send('Server Error');
       }
+      
 };
 
 module.exports = {createTable, register, login};
